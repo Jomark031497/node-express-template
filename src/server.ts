e47beca80ff33aa42f043c0f6b1b64e8c6a9f3fe
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-07-05 10:23:07 +0800

    removed

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import logger from './utils/logger';
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
+
 app.listen(port, () => {
   logger.info('server started');
 });
